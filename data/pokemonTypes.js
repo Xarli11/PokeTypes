fef3d2eb945cac2afa61024d723439a0ee42a6ad
Xarli11 <carlosgt2001@icloud.com>
2026-01-02 14:13:57 +0100

    refactor: remove 'Light' type and update Pokémon names for consistency

--- data/pokemonTypes.js
+++ data/pokemonTypes.js
@@ -1,6 +1,6 @@
 // Definition of all Pokémon types in the desired order for the table
  const POKEMON_TYPES = [
     'Normal', 'Fire', 'Water', 'Grass', 'Electric', 'Ice', 'Fighting',
-    'Poison', 'Ground', 'Flying', 'Psychic', 'Bug', 'Rock', 'Light',
+    'Poison', 'Ground', 'Flying', 'Psychic', 'Bug', 'Rock',
     'Ghost', 'Dragon', 'Steel', 'Fairy', 'Dark'
 ];
