b32ac7a4fb264292515f705c5feebefb2e633f62
Xarli11 <carlos.garcia@example.com>
2026-01-29 00:49:02 +0100

    fix(calc): split resistances into 0.5x and 0.25x groups for better clarity

--- src/js/main.js
+++ src/js/main.js
@@ -285,7 +285,7 @@ function displayAnalysis(t1, t2) {
     // Render Cards
     ui.renderSplitEffectivenessCard(document.getElementById('weaknesses'), 'Weak to', def.weaknesses4x, def.weaknesses2x, 'None', 'super', appData.contrast);
     ui.renderEffectivenessCard(document.getElementById('neutral-damage'), 'Neutral Damage', def.neutral, 'None', 'neutral', appData.contrast);
-    ui.renderEffectivenessCard(document.getElementById('resistances'), 'Resistances', def.resistances, 'None', 'resist', appData.contrast);
+    ui.renderSplitResistanceCard(document.getElementById('resistances'), 'Resistances', def.resistances025x, def.resistances05x, 'None', 'resist', appData.contrast);
     ui.renderEffectivenessCard(document.getElementById('immunities'), 'Immunities', def.immunities, 'None', 'immune', appData.contrast);
 
     // AI Advisor
