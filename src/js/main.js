d5918adb41b34e68da785495e498c001efec8ff3
Xarli11 <carlos.garcia@example.com>
2026-01-29 01:33:47 +0100

    chore: release v2.17.3

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-import { loadAppData, fetchPokemonDetails } from './modules/data.js?v=2.17.1';
-import { calculateDefense, calculateOffense, findImmuneDualTypes } from './modules/calculator.js?v=2.17.1';
-import { getTacticalAdvice } from './modules/advisor.js?v=2.17.1';
-import * as ui from './modules/ui.js?v=2.17.1';
-import { initTheme } from './modules/theme.js?v=2.17.1';
+import { loadAppData, fetchPokemonDetails } from './modules/data.js?v=2.17.3';
+import { calculateDefense, calculateOffense, findImmuneDualTypes } from './modules/calculator.js?v=2.17.3';
+import { getTacticalAdvice } from './modules/advisor.js?v=2.17.3';
+import * as ui from './modules/ui.js?v=2.17.3';
+import { initTheme } from './modules/theme.js?v=2.17.3';
 import html2canvas from './libs/html2canvas.js';
 
 let appData = null;
